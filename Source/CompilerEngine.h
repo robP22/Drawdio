e3281ff9ebb62152e9796fc581cd590b9afaa08f
robP <160159668+robP22@users.noreply.github.com>
2026-06-03 00:43:16 -0700

    Delete Source directory

--- Source/CompilerEngine.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <cstdint>
-#include <vector>
-#include "PedalStructures.h"
-
-PedalAssetPayload compileCanvas(const uint8_t* gridData,
-                                const std::vector<DspModuleType>& pedalSlots,
-                                const std::vector<uint8_t>& manualRouting = {},
-                                const std::vector<ParameterDescriptor>& existingParams = {});
